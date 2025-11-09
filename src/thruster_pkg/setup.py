fa24fc6d3b95d3555be3a01f4aff7e605a8b57fa
nu-wave <neumate2022@gmail.com>
2025-11-09 16:11:41 -0500

    Create thruster pkg

---
+++ src/thruster_pkg/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'thruster_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nuwave',
+    maintainer_email='nuwave@todo.todo',
+    description='package for thrusters',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'thruster_node = '+package_name+'.thruster_driver:main',
+            'thruster_pub = '+package_name+'.thruster_pub:main'
+        ],
+    },
+)
