e487bd10b6345513eefdbc799baaa9a66a3f9bca
cwcinc <zifcak.c@northeastern.edu>
2026-03-22 16:51:51 -0400

    add master_setup.sh

---
+++ master_setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+WS="$HOME/nuwave-rov"
+
+# Build first
+echo "Running colcon build..."
+cd "$WS" && colcon build
+if [ $? -ne 0 ]; then
+    echo "colcon build failed!"
+    exit 1
+fi
+
+SETUP="cd $WS && source install/setup.bash"
+
+CMD_TL="$SETUP && ros2 launch houston_pkg joystick_launch.launch.py"
+CMD_TR="$SETUP && ros2 run houston_pkg houston"
+CMD_BL="$SETUP && ros2 run controller thruster_controller_node"
+CMD_BR="$SETUP && echo 'hello'"
+
+LAYOUT_FILE=$(mktemp /tmp/terminator_ros2_XXXX.conf)
+
+cat > "$LAYOUT_FILE" <<EOF
+[layouts]
+  [[ros2]]
+    [[[window0]]]
+      type = Window
+      parent = ""
+    [[[hpane]]]
+      type = HPaned
+      parent = window0
+    [[[vpane_left]]]
+      type = VPaned
+      parent = hpane
+    [[[vpane_right]]]
+      type = VPaned
+      parent = hpane
+    [[[top-left]]]
+      type = Terminal
+      parent = vpane_left
+      command = bash -c '$CMD_TL; exec bash'
+    [[[bottom-left]]]
+      type = Terminal
+      parent = vpane_left
+      command = bash -c '$CMD_BL; exec bash'
+    [[[top-right]]]
+      type = Terminal
+      parent = vpane_right
+      command = bash -c '$CMD_TR; exec bash'
+    [[[bottom-right]]]
+      type = Terminal
+      parent = vpane_right
+      command = bash -c '$CMD_BR; exec bash'
+EOF
+
+terminator -g "$LAYOUT_FILE" -l ros2 -m
+rm -f "$LAYOUT_FILE"
