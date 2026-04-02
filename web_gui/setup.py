30bb12a61129fbffdf4cf80ab8c2e9c316db55c2
cwcinc <zifcak.c@northeastern.edu>
2026-04-02 03:06:13 -0400

    init node

---
+++ web_gui/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'web_gui'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Cameron Zifcak',
+    maintainer_email='cameronzifcak@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'bridge_node = web_gui.bridge_node:main'
+        ],
+    },
+)
