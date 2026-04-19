b91b7a1bcd526f12e6c438acbc1877f3583ceba4
nu-wave <neumate2022@gmail.com>
2026-04-19 16:02:42 -0400

    fix joystick launch in a way that's hopefully robust this time

--- src/houston_pkg/launch/joystick_launch.launch.py
+++ src/houston_pkg/launch/joystick_launch.launch.py
@@ -1,7 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import subprocess
 
 def generate_launch_description():
+    result = subprocess.check_output(['ros2', 'run', 'joy', 'joy_enumerate_devices'])
+    result_word1 = result.split(b'\n')[2].split(b':')[-1].lstrip()
+    result_word2 = result.split(b'\n')[3].split(b':')[-1].lstrip()
+    rov_joy_id = -1
+    arm_joy_id = -1
+    if result_word1[0] == 80:
+        rov_joy_id = 0
+    elif result_word1[0] == 88:
+        arm_joy_id = 0
+    if result_word2[0] == 80:
+        rov_joy_id = 1
+    elif result_word2[0] == 88:
+        arm_joy_id = 1
     return LaunchDescription([
         Node(
             package='joy',
@@ -11,8 +25,8 @@ def generate_launch_description():
                 ('/joy', '/joy_thruster')
             ],
             parameters=[{
-                'device_name': 'PowerA Xbox Series X Controller',
-                # 'device_id': 0
+                # 'device_name': 'PowerA Xbox Series X Controller',
+                'device_id': rov_joy_id
             }]
 
         ),
@@ -25,7 +39,7 @@ def generate_launch_description():
             ],
             parameters=[{
                 # 'device_name': 'X360 Controller',
-                # 'device_id': 1
+                'device_id': arm_joy_id
             }]
         ),
     ])
