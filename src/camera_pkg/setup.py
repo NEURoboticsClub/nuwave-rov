c0993dc3c76f26787197bb5b8c64805ed87a4288
cwcinc <zifcak.c@northeastern.edu>
2026-05-09 19:09:29 -0400

    fix launch package setup

--- src/camera_pkg/setup.py
+++ src/camera_pkg/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch/multicamera_launch'),
+        ('share/' + package_name + '/launch/multi_camera_launch', ['launch/multi_camera_launch.launch.py']),
     ],
     install_requires=['setuptools', 'cv2-enumerate-cameras'],
     zip_safe=True,
